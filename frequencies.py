be75f23e1c1e6008fd446919131a6d9223a92b50
shivam <86538330+shivam-909@users.noreply.github.com>
2022-10-20 22:26:59 +0100

    frequencies: Implement solution

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,9 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    l = lambda x: str(x)
+    items = list(map(l, items))
+
+    for x in range(0, len(items)):
+        frequencies[str(items[x])] = items.count(items[x])
     return frequencies
